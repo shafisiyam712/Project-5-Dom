09a526158f72adbce1f14f49a98034b74deeb28d
shafisiyam712 <shafisiyam674.com>
2024-09-25 01:45:34 +0600

    sticky bar done

--- Js/blog.js
+++ Js/blog.js
@@ -3,8 +3,4 @@ document.getElementById('blog-btn').addEventListener('click',function(even){
  window.location.href='./blog.html'
     
 })
-// document.getElementById('home-btn').addEventListener('click',function(){
-//  window.location.href='./index.html'
-//     console.log('home btn clicked');
-    
-// })
+
